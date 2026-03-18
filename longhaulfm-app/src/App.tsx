7f5150fbbbb4fd1f020c9d51f364a91dc3f38638
longhaulfm-creator <longhaulfm@gmail.com>
2026-03-18 10:38:46 +0200

    Update App.tsx

--- longhaulfm-app/src/App.tsx
+++ longhaulfm-app/src/App.tsx
@@ -1,1 +1,7 @@
-// App.tsx
+// src/App.tsx
+import { RouterProvider } from '@tanstack/react-router'
+import { router } from './router'
+
+export default function App() {
+  return <RouterProvider router={router} />
+}
