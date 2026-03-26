7173ea990d271bffc67eab9470a69be8d2ace7ad
longhaulfm-creator <longhaulfm@gmail.com>
2026-03-26 13:57:55 +0200

    Update main.tsx

--- longhaulfm-app/src/main.tsx
+++ longhaulfm-app/src/main.tsx
@@ -1,6 +1,7 @@
 // src/main.tsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import './i18n/config'  // ← add this line before App
 import App from './App'
 import './styles/globals.css'
 
